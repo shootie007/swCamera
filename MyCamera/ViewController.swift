b28720617319d91d1c3da710b52e2d2db0eaf4a6
多田秀人 <shoot@Shootie-Air.local>
2020-09-10 17:15:32 +0900

    first commit

---
+++ MyCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCamera
+//
+//  Created by 多田秀人 on 2020/09/10.
+//  Copyright © 2020 多田秀人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
